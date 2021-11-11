d127c2597591309ac559ffed642fee48edb95722
david rose <aussiecoder0@gmail.com>
2021-11-11 20:10:03 +1100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+input.onPinPressed(TouchPin.P0, function () {
+    score += 1
+    basic.showIcon(IconNames.Yes)
+    basic.pause(500)
+    basic.showNumber(score)
+})
+input.onButtonPressed(Button.A, function () {
+    score = 0
+})
+let score = 0
+score = 0
